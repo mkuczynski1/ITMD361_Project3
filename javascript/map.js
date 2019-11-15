a28f6b07e3d5c3e871776a4d60b36f0e00226964
Michael Kuczynski <mkuczynski@outlook.com>
2019-11-14 22:03:33 -0600

    Map added
    Map added on map page, centered on IIT

---
+++ javascript/map.js
@@ -0,0 +1,10 @@
+// AIzaSyB606_aI4rTlN4SwiUMGw4DFQ3EjFdQmqc - Google API Key
+
+var map;
+
+function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 41.834849, lng: -87.626984},
+          zoom: 8
+        });
+      }
