45282347ddb88ed99f424b386896168dbfa0e84a
Michael Kuczynski <mkuczynski@outlook.com>
2019-11-28 22:51:24 -0600

    fixed missing requirements
    slideshow on pets page, fixed media queries, spacing set to 2, small other edits made

---
+++ javascript/slideshow.js
@@ -0,0 +1,18 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(x) {
+  showSlides(slideIndex += x);
+}
+function currentSlide(x) {
+  showSlides(slideIndex = x);
+}
+function showSlides(x) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (x > slides.length) {slideIndex = 1}
+  if (x < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
